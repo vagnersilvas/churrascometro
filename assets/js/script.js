95ba254f2e4bc810386cea2d00d5f9c00f4bcf4e
vagner da silva <vagnrdaia@gmail.com>
2022-02-17 22:04:18 -0300

    refatoração codigo javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,49 +14,122 @@
 // Math.ceil(adultos * 2000 / 350)
 // Litros refrigerante/ agua
 // (adultos * 1000 + crianças * 1000 / 2)/ 1000 
-
-
-function carne(adultos, crianças, horas) {
-    let carneChurras = 0
-    if (horas >= 6) {
-        carneChurras = (adultos * 0.65 + (crianças * 0.65) / 2).toFixed(2)
-    } else {
-        carneChurras = (adultos * 0.5 + crianças * 0.5 / 2).toFixed(2)
-    }
-    return carneChurras
+const itensAdulto = {
+    carne: {
+        maisSeisHoras: 0.65,
+        menosSeisHoras: 0.4
+    },
+    cerveja: {
+        maisSeisHoras: 2300,
+        menosSeisHoras: 1500
+    },
+    refrigerante: {
+        maisSeisHoras: 1500,
+        menosSeisHoras: 1000
+    },
+}
+const itensCrianca = {
+    carne: {
+        maisSeisHoras: 0.325,
+        menosSeisHoras: 0.2
+    },
+    refrigerante: {
+        maisSeisHoras: 750,
+        menosSeisHoras: 500
+    },
 }
 
-function cerveja(adultos, horas) {
-    let latasDeCerveja = 0
-    if (horas >= 6) {
-        latasDeCerveja = Math.ceil(adultos * 2300 / 355)
-    } else {
-        latasDeCerveja = Math.ceil(adultos * 1500 / 355)
-    }
-    return latasDeCerveja
+const elements = {
+    description: document.querySelector('.group-results h2'),
+    resultCarne: document.getElementById("carne"),
+    resultCerveja: document.getElementById("cerveja"),
+    resultRefrigerante: document.getElementById("refrigerante"),
 }
 
-function refrigerante(adultos, crianças, horas) {
-    let litrosRefrigerante = 0
-    if(horas >= 6) {
-        litrosRefrigerante = Math.ceil((adultos * 1500 + crianças * 1500 / 2) / 2000)
-    }else {
-        litrosRefrigerante = Math.ceil((adultos * 1500 + crianças * 1000 / 2) / 2000)
-    }
-    return litrosRefrigerante
+const somar = (a, b) => (a + b).toFixed(2)
+
+function carnePorQtdPessoas(qtdPessoas, percentual) {
+    return qtdPessoas * percentual;
 }
-function calcular(){
-    let inputAdultos = document.getElementById("adultos").value
-    let inputCriancas = document.getElementById("crianças").value
-    let inputHoras = document.getElementById("horas").value
-    
-    let description = document.querySelector('.group-results h2')
-    let resultCarne = document.getElementById("carne")
-    let resultCerveja = document.getElementById("cerveja")
-    let resultRefrigerante = document.getElementById("refrigerante")
+
+function calcKgCarne(pessoa, horas, itens) {
+    const qtdCarne = horas >= 6
+        ? itens.carne.maisSeisHoras
+        : itens.carne.menosSeisHoras
+
+    return carnePorQtdPessoas(pessoa, qtdCarne)
+}
+
+
+function calculaQtdCerveja(pessoas, mlCerveja) {
+    return Math.ceil(pessoas * mlCerveja / 355)
+}
+
+function latasCerveja(adultos, horas) {
+    const { cerveja } = itensAdulto
+
+    const mlCerveja = horas >= 6
+        ? cerveja.maisSeisHoras
+        : cerveja.menosSeisHoras
+
+    return calculaQtdCerveja(adultos, mlCerveja)
+}
+
+function calcRefrigerante(pessoa, mlRefrigerante) {
+    return Math.ceil((pessoa * mlRefrigerante) / 2000)
+}
+
+function qtdeRefrigerante(pessoa, horas, itens) {
+    const mlRefrigerante = horas >= 6
+        ? itens.refrigerante.maisSeisHoras
+        : itens.refrigerante.menosSeisHoras
+
+    return calcRefrigerante(pessoa, mlRefrigerante)
+}
+
+
+
+
+function exibeNoDocument(carne, cerveja, refrigerante) {
+
+    const { description, resultCarne, resultCerveja, resultRefrigerante } = elements
 
     description.innerHTML = `Para o seu churrasco recomendamos`
-    resultCarne.innerHTML = `<img src="./assets/img/meat.png"> <span>${carne(inputAdultos, inputCriancas, inputHoras)} Kg</span> de carne`
-    resultCerveja.innerHTML = `<img src="./assets/img/beer.png"> <span>${cerveja(inputAdultos, inputHoras)} Latas</span> de cerveja`
-    resultRefrigerante.innerHTML = `<img src="./assets/img/cola.png"> <span>${refrigerante(inputAdultos, inputCriancas, inputHoras)} L de</span> refrigerante`
+    resultCarne.innerHTML = `<img src="./assets/img/meat.png"> <span>${carne} Kg</span> de carne`
+    resultCerveja.innerHTML = `<img src="./assets/img/beer.png"> <span>${cerveja} Latas</span> de cerveja`
+    resultRefrigerante.innerHTML = `<img src="./assets/img/cola.png"> <span>${refrigerante} L de</span> refrigerante`
+
+}
+
+function getValueInput(id) {
+    return document.getElementById(id).value
+}
+
+function calcTotal() {
+    const adultos = getValueInput("adultos");
+    const criancas = getValueInput("criancas");
+    const horas = getValueInput("horas");
+
+
+    const kgCarneAdultos = calcKgCarne(adultos, horas, itensAdulto)
+    const kgCarneCrianca = calcKgCarne(criancas, horas, itensCrianca)
+
+    const mlRefrigeranteAdulto = qtdeRefrigerante(adultos, horas, itensAdulto)
+    const mlRefrigeranteCrianca = qtdeRefrigerante(criancas, horas, itensCrianca)
+
+    const totalCerveja = latasCerveja(adultos, horas)
+
+    const totalCarne = somar(kgCarneCrianca, kgCarneAdultos)
+    const totalRefrigerante = somar(mlRefrigeranteAdulto, mlRefrigeranteCrianca)
+
+    return [totalCarne, totalRefrigerante, totalCerveja]
+
+}
+
+function exibir() {
+
+    const [carne, refrigerante, cerveja] = calcTotal()
+
+    exibeNoDocument(carne, cerveja, refrigerante);
+
 }
